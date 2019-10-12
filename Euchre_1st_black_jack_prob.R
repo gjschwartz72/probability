4344f53ad3f8c2d86c57aa54a3c43e5a35811891
gjs <me@me.com>
2019-10-12 12:23:55 -0700

    added cool example with recursive generation of a probability distribution

--- Euchre_1st_black_jack_prob.R
+++ Euchre_1st_black_jack_prob.R
@@ -1,4 +1,13 @@
-propX = function(nCards, nPass){
+# Note the edge cases (nPass < 1) or nPass > nCards is taken care of.
+# Both cases return 0.
+
+# Function returns the probability of dealy a black jack from the
+# standard Eucher deck on the <nPass> deal.
+# For example, 
+#   P(nPass == 1) = 2 / 24 
+#   P(nPass == 2) = (23 / 24) * (2 / 23) = 1/12
+probX = function(nCards, nPass){
+  if (nPass < 1) return(0)
   if (nPass == 1) {
     return(2 / nCards)
   }
@@ -6,12 +15,14 @@ propX = function(nCards, nPass){
     # Note: when nCards = 2, p == 0 which is expected as
     # this is the number of non-black jacks.
     # It is not possible to deal 24 cards
-    p = (nCards - 2) / nCards * propX(nCards - 1, nPass - 1)
+    p = (nCards - 2) / nCards * probX(nCards - 1, nPass - 1)
+    
+    if(is.na(p)) p = 0
     return(p)
   }
 }
 
-dist = sapply(1:24, FUN = function(n) {propX(24, n)})
+dist = sapply(1:24, FUN = function(n) {probX(24, n)})
 
 player_probs = cbind(dist, 1:length(dist) %% 4)
 
